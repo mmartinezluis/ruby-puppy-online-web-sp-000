fe26dd7ac4ac65991759a326b5131942f0fd4ba9
Luis Martinez <luis_mmartinez@live.com>
2021-01-01 17:55:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,33 @@
-# Add your code here
+require "pry"
+  class Dog
+
+  @@all = [ ]
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name= name
+    save
+  end
+
+  def self.all
+    @@all
+
+  end
+
+  def self.clear_all
+    @@all= [ ]
+    @@all
+  end
+
+  def self.print_all
+    puts @@all
+    binding.pry
+
+  end
+
+  def save
+    @@all << self
+  end
+
+end
